ca7949b858e27eeab88a7d2b02a0ec6370a914f5
Jeronimo Romero Tricarico <jerotrica@gmail.com>
2016-10-31 09:43:49 -0300

    Primer Commit

---
+++ test/ar/edu/unlam/pb2/TestAnimales.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAnimales {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
